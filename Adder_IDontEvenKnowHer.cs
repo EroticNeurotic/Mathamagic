20b01f505bc4c51dc946fcf7c2e1fd7713ecbb74
EroticNeurotic <MissAmerica2k18@yahoo.com>
2018-03-23 12:56:19 +0000

    The start of a very pointless journey in binary

---
+++ Adder_IDontEvenKnowHer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static bool[] FullAdder(bool A, bool B, bool C)
+        {
+            bool[] HalfResult = HalfAdder(A, B);
+
+            bool Result = new bool();
+            bool Carry = new bool();
+
+            Result = HalfResult[0] ^ C;
+            Carry = HalfResult[1] || (C & HalfResult[0]);
+
+            return new bool[] { Result, Carry };
+        }
+
+        static bool[] HalfAdder(bool A, bool B)
+        {
+            bool sum = new bool();
+            bool carry = new bool();
+
+            sum = A ^ B;
+            carry = A & B;
+
+            return new bool[] { sum, carry };
+        }
+    }
+}
