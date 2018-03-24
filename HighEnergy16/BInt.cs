9e81be2bbb93f44fb7ebb86fa364ccc1af4617f3
EroticNeurotic <MissAmerica2k18@yahoo.com>
2018-03-24 19:04:57 +0000

    Rough and tumble binary silliness

---
+++ HighEnergy16/BInt.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathFunctions
+{
+    class BInt
+    {
+        public bool[] binary;
+        public const int bits = 16;
+        private Dictionary<char, bool[]> digits;
+
+        public BInt ()  //really shouldn't be using int but I haven't found a way around it yet
+        {
+            binary = new bool[bits];
+            digits = valueDictionary();
+        }
+
+        public void SetValue (string val)
+        {
+            val = Reverse(val);
+            int position = new int();
+            int length = val.Length;
+
+            bool[] temp = new bool[bits];
+            bool[] value = new bool[bits];
+            bool[] ten = digits['0'];
+            bool[] multiplier = new bool[bits];
+
+            for(int count = 0; count < length; count ++)
+            {
+                multiplier = digits['1'];
+                position = count + 1;
+
+                if (position > 1)
+                {
+                    for (int index = 1; index < position; index++)
+                    {
+                        multiplier = Operations.Multiplication(ten, multiplier, bits);
+                    }
+                }
+
+                temp = digits[val[count]];
+                temp = Operations.Multiplication(temp, multiplier, bits);
+                value = Operations.Addition(value, temp, bits);
+            }
+            binary = value;
+
+        }
+
+        public static void printValue(bool[] value)
+        { 
+            foreach (bool digit in value)
+            {
+                Console.Write(digit + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private Dictionary<char, bool[]> valueDictionary()
+        {
+            Dictionary<char, bool[]> digits = new Dictionary<char, bool[]>();   //find a way to add only the necessary digits and set the rest automatically to false
+          
+            digits.Add('1', new bool[bits] { true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false});
+            digits.Add('2', new bool[bits] { false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('3', new bool[bits] { true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('4', new bool[bits] { false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('5', new bool[bits] { true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('6', new bool[bits] { false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('7', new bool[bits] { true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('8', new bool[bits] { false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('9', new bool[bits] { true, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false });
+            digits.Add('0', new bool[bits] { false, true, false, true, false, false, false, false, false, false, false, false, false, false, false, false });
+
+            return digits;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as BInt);
+        }
+
+        public bool Equals(BInt B)
+        {
+            if (Object.ReferenceEquals(B, null)) { return false; }
+            if (Object.ReferenceEquals(B, this)) { return true; }
+            if (this.GetType() != B.GetType()) { return false; }
+            return binary == B.binary;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(BInt Left, BInt Right)
+        {
+            if (object.ReferenceEquals(Left, null))
+            {
+                if (object.ReferenceEquals(Right, null)){ return true; }
+                return false;
+            }
+            return Left.Equals(Right);
+        }
+
+        public static bool operator !=(BInt Left, BInt Right)
+        {
+            return !(Left == Right);
+        }
+
+    }
+}
---
+++ HighEnergy16/Operations.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace MathFunctions
+{
+    class Operations
+    {
+
+        public static bool[] Multiplication(bool[] first, bool[] second, int bits)
+        {
+            bool[] result = new bool[bits];
+            bool[] temp = new bool[bits];
+
+            for(int count = 0; count < bits; count ++)
+            {
+                if(first[count] == true)
+                {
+                    temp = second;
+
+                    if(count != 0)
+                    {
+                        for(int index = 1; index <= count; index ++)
+                        {
+                            temp = ShiftRight(temp, bits);
+                        }
+                    }
+
+                    result = Addition(result, temp, bits);
+                }
+
+            }
+
+            return result;
+
+        }
+
+        public static bool[] ShiftRight(bool[] barray, int bits)
+        {
+            bool[] result = new bool[bits];
+
+            Array.Copy(barray, result, bits);
+
+            for(int count = 1; count < bits; count ++)
+            {
+                result[count] = barray[count - 1];
+            }
+            result[0] = false;
+
+            return result;
+        }
+
+        public static bool[] Addition(bool[] first, bool[] second, int bits)
+        {
+
+            bool[] result = new bool[bits];
+            bool[] temp = new bool[] { false, false };
+            bool carry = false;
+
+            for (int count = 0; count < bits; count++)    //for loops are cheating, uses ints and addition (while loops?)
+            {
+                temp = FullAdder(first[count], second[count], carry);
+                result[count] = temp[0];
+                carry = temp[1];
+            }
+
+            if (carry == true)
+            {
+                Console.WriteLine("Overflow Error");
+            }
+
+            return result;
+        }
+
+        static bool[] FullAdder(bool A, bool B, bool C)
+        {
+            bool[] HalfResult = HalfAdder(A, B);
+
+            bool Result = new bool();
+            bool Carry = new bool();
+
+            Result = HalfResult[0] ^ C;
+            Carry = HalfResult[1] || (C & HalfResult[0]);
+
+            return new bool[] { Result, Carry };
+        }
+
+        static bool[] HalfAdder(bool A, bool B)
+        {
+            bool sum = new bool();
+            bool carry = new bool();
+
+            sum = A ^ B;
+            carry = A & B;
+
+            return new bool[] { sum, carry };
+        }
+
+        public static void PrintResult(bool[] Answer)
+        {
+            foreach (bool element in Answer)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
